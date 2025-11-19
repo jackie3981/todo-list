b18e61583f5a61228cd14d129a62f9ea1c210b70
jackie <jackie.3981@gmail.com>
2025-11-19 11:12:27 -0600

    Week 02 - Created two new components (TodoList, TodoForm)

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import "./App.css";
+import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
 
 function App() {
-  const todos = [
-    { id: 1, title: "review resources" },
-    { id: 2, title: "take notes" },
-    { id: 3, title: "code out app" },
-  ];
+  
 
   return (
     <div>
       <h1>My Todos</h1>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
-        ))}
-      </ul>
+      <TodoForm />
+      <TodoList />
+      
     </div>
   );
 }
