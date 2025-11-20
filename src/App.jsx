b8b6af72efca08c2eed742fe3772f51a9d80f20e
jackie <jackie.3981@gmail.com>
2025-11-19 20:03:42 -0600

    Lesson 03 - Adding useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import "./App.css";
+import { useState } from "react";
 import TodoList from "./components/TodoList";
 import TodoForm from "./components/TodoForm";
+import { todos } from "./data/todos";
+import "./App.css";
 
 function App() {
-  
+  const [todoList, setTodoList] = useState(todos);
 
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm />
-      <TodoList />
-      
+      <TodoList todoList={todoList} />
     </div>
   );
 }
---
+++ src/data/todos.js
@@ -0,0 +1,5 @@
+export const todos = [
+    { id: 1, title: "review resources" },
+    { id: 2, title: "take notes" },
+    { id: 3, title: "code out app" },
+];
