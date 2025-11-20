9f0cbca81ae9a3c6f8ebdcc57a17e32d9730b914
jackie <jackie.3981@gmail.com>
2025-11-20 15:32:33 -0600

    Week05 - Hooks, Events, Handlers

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import { useState } from "react";
 import TodoList from "./components/TodoList";
 import TodoForm from "./components/TodoForm";
-import { todos } from "./data/todos";
+//import { todos } from "./data/todos";
 import "./App.css";
 
 function App() {
-  const [todoList, setTodoList] = useState(todos);
+  const [todoList, setTodoList] = useState([]);
+
+  function addTodo(todoTitle) {
+    const newTodo = {
+      id: Date.now(),
+      title: todoTitle,
+    };
+
+    setTodoList([newTodo, ...todoList]);
+  }
 
   return (
     <div>
       <h1>My Todos</h1>
-      <TodoForm />
+      <TodoForm onAddTodo={addTodo} />
       <TodoList todoList={todoList} />
     </div>
   );
